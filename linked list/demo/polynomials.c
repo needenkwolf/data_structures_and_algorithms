23660fc07f8989df96d782d65883676e6357ff08
needenkwolf <viridiansilverwolf@gmail.com>
2026-05-20 19:33:14 -0300

    improvements on multiplication and division. some comments

--- linked list/demo/polynomials.c
+++ linked list/demo/polynomials.c
@@ -18,12 +18,15 @@ struct lnode *mulPlynExpr(struct lnode *expr1, struct lnode *expr2);
 struct lnode *divPlynExpr(struct lnode *expr1, struct lnode *expr2);
 struct lnode *simplifyPlynExpr(struct lnode **expr);
 
+void sortArray(int *array, int n);
+
 void printExpr(struct lnode *expr);
 
 int main(int argc, char *argv[]) 
 {
-	struct lnode *expr1 = createPlynExpr(9, 3);
-	insertPlynTerm(&expr1, 6, 2);
+	struct lnode *expr1 = createPlynExpr(36, 7);
+	
+	insertPlynTerm(&expr1, 18, 6);
 	/*
 	insertPlynTerm(&expr1, 8, 3);
 	insertPlynTerm(&expr1, 16, 1);
@@ -32,11 +35,13 @@ int main(int argc, char *argv[])
 	printf("first expr:\t");
 	printExpr(expr1);
 
-	struct lnode *expr2 = createPlynExpr(3, 2);
-	/*
-	insertPlynTerm(&expr2, 16, 1);
-	insertPlynTerm(&expr2, 9, 0);
-	*/
+	struct lnode *expr2 = createPlynExpr(6, 5);
+	insertPlynTerm(&expr2, 3, 4);
+	insertPlynTerm(&expr2, 9, 3);
+	insertPlynTerm(&expr2, 9, 2);
+	insertPlynTerm(&expr2, 7, 1);
+	insertPlynTerm(&expr2, 4, 0);
+	
 	printf("second expr:\t");
 	printExpr(expr2);
 
@@ -52,11 +57,19 @@ int main(int argc, char *argv[])
 
 	struct lnode *mul = mulPlynExpr(expr1, expr2);
 	printf("mul:\t\t");
-	printExpr(mul);
+	if (mul == NULL) {
+		printf("unsupported\n");
+	} else {
+		printExpr(mul);
+	}
 
 	struct lnode *div = divPlynExpr(expr1, expr2);
 	printf("div:\t\t");
-	printExpr(div);
+	if (div == NULL) {
+		printf("unsupported\n");
+	} else {
+		printExpr(div);
+	}
 	return 0;
 }
 
@@ -76,12 +89,19 @@ struct lnode *insertPlynTerm(struct lnode **expr, int coeff, int power)
 	return insertAtLend(expr, (void*)term);
 }
 
+/*	function to add polynomials expressions
+ *	works as intended for most polynomials
+ *	probably won't change much in the future
+*/
 struct lnode *addPlynExpr(struct lnode *expr1, struct lnode *expr2)
 {
 	struct lnode *sum = NULL;
-	while (expr1 != NULL && expr2 != NULL) {
-		struct plyn *term1 = (struct plyn*)expr1->data;
-		struct plyn *term2 = (struct plyn*)expr2->data;
+
+	struct lnode *p = expr1;
+	struct lnode *q = expr2;
+	while (p != NULL && q != NULL) {
+		struct plyn *term1 = (struct plyn*)p->data;
+		struct plyn *term2 = (struct plyn*)q->data;
 
 		if (term1->power == term2->power) {
 			int res = term1->coeff + term2->coeff;
@@ -97,7 +117,7 @@ struct lnode *addPlynExpr(struct lnode *expr1, struct lnode *expr2)
 				} else {
 					insertPlynTerm(&sum, term1->coeff, term1->power);
 				}
-				expr1 = expr1->next;
+				p = p->next;
 				continue;
 			} else {
 				if (sum == NULL) {
@@ -105,20 +125,35 @@ struct lnode *addPlynExpr(struct lnode *expr1, struct lnode *expr2)
 				} else {
 					insertPlynTerm(&sum, term2->coeff, term2->power);
 				}
-				expr2 = expr2->next;
+				q = q->next;
 				continue;
 			}
 		}
-		
-		expr1 = expr1->next;
-		expr2 = expr2->next;
+		p = p->next;
+		q = q->next;
+	}
+	while (p != NULL && sum != NULL) {
+		struct plyn *term1 = (struct plyn*)p->data;
+		insertPlynTerm(&sum, term1->coeff, term1->power);
+		p = p->next;
+	}
+	while (q != NULL && sum != NULL) {
+		struct plyn *term2 = (struct plyn*)q->data;
+		insertPlynTerm(&sum, term2->coeff, term2->power);
+		q = q->next;
 	}
 	simplifyPlynExpr(&sum);
 	return sum;
 }
 
+/*	function to subtract polynomials expressions
+ *	works as intended for most polynomials
+ *	might be optimized yet
+*/
 struct lnode *subPlynExpr(struct lnode *expr1, struct lnode *expr2)
 {
+	if (expr1 == NULL || expr2 == NULL) return NULL;
+
 	struct lnode *p = expr2;
 	while (p != NULL) {
 		struct plyn *term2 = (struct plyn*)p->data;
@@ -137,65 +172,205 @@ struct lnode *subPlynExpr(struct lnode *expr1, struct lnode *expr2)
 	return sub;
 }
 
+/*	w.i.p function to multiply a polynomial expression
+ *	not fully supported yet (20/05/2026)
+ *	mostly correct if size(expr1) <= 2.
+ *	still needs to be fixed for size(expr1) > 2
+*/
 struct lnode *mulPlynExpr(struct lnode *expr1, struct lnode *expr2)
 {
+	if (getLsize(expr1) > 2) return NULL;
+
 	struct lnode *mul = NULL;
 
-	while (expr1 != NULL && expr2 != NULL) {
-		struct plyn *term1 = (struct plyn*)expr1->data;
-		struct plyn *term2 = (struct plyn*)expr2->data;
+	struct lnode *p = expr1;
+	struct lnode *q = expr2;
+	while (p != NULL && q != NULL) {
+		struct plyn *term1 = (struct plyn*)p->data;
+		struct plyn *term2 = (struct plyn*)q->data;
 
-		if (term1->power >= term2->power) {
-			int coeff = term1->coeff * term2->coeff;
-			int power = term1->power + term2->power;
+		int coeff = term1->coeff * term2->coeff;
+		int power = term1->power + term2->power;
 
-			if (mul == NULL) {
-				mul = createPlynExpr(coeff, power);
-			} else {
-				insertPlynTerm(&mul, coeff, power);
-			}
-
-			expr1 = expr1->next;
-			continue;
+		if (mul == NULL) {
+			mul = createPlynExpr(coeff, power);
+		} else {
+			insertPlynTerm(&mul, coeff, power);
 		}
-
-		expr1 = expr1->next;
-		expr2 = expr2->next;
+		p = p->next;
+		if (p == NULL && q != NULL) {
+			p = expr1;
+			q = q->next;
+			continue;
+		} 
 	}
-	
+	simplifyPlynExpr(&mul);
 	return mul;
 }
 
+/* 	w.i.p function to divide a polynomial expression
+ *  	not fully supported yet (20/05/2026)
+ * 	works fine as long as size(expr1) and size(expr2) are both <= 2.
+ * 	AND
+ * 	expr1 is divisible by expr2 as these lists have integer data
+ * 	bigger expressions aren't supported yet
+*/
 struct lnode *divPlynExpr(struct lnode *expr1, struct lnode *expr2)
 {
-	struct lnode *div = NULL;
+	if (getLsize(expr1) > 2) return NULL;
+	if (getLsize(expr2) > 2) return NULL;
 
-	while (expr1 != NULL && expr2 != NULL) {
-		struct plyn *term1 = (struct plyn*)expr1->data;
-		struct plyn *term2 = (struct plyn*)expr2->data;
-	
-		if (term1->power >= term2->power) {
+	struct lnode *div = NULL;
+	struct lnode *mul = NULL;
+	struct lnode *sub = NULL;
+	struct lnode *tosub = NULL;
+
+	int i = 1;
+	int found = 0;
+	struct lnode *p = expr1;
+	struct lnode *q = expr2;
+	while (p != NULL && q != NULL && found == 0) {
+		struct plyn *term1 = (struct plyn*)p->data;
+		struct plyn *term2 = (struct plyn*)q->data;
+
+		if (i == 1) {
+			if (term1->coeff % term2->coeff != 0) break;
 			int coeff = term1->coeff / term2->coeff;
 			int power = term1->power - term2->power;
-
-			if (div == NULL) {
-				div = createPlynExpr(coeff, power);
+			
+			div = createPlynExpr(coeff, power);
+		} else {
+			if (tosub == NULL) {
+				tosub = expr1;
 			} else {
-				insertPlynTerm(&div, coeff, power);
+				tosub = sub;
 			}
+			if (mul != NULL) freeList(&mul);
+			if (sub != NULL) freeList(&sub);
 
-			expr1 = expr1->next;
-			continue;
+			mul = mulPlynExpr(expr2, div);
+			if (mul == NULL) return NULL;
+
+			sub = subPlynExpr(tosub, mul);
+
+			struct plyn *subterm = (struct plyn*)sub->data;
+			struct lnode *subp = sub;
+
+			found = 1;
+			while (subp != NULL) {
+				subterm = (struct plyn*)subp->data;
+				if (subterm->coeff != 0) {
+					found = 0;
+				}
+				subp = subp->next;
+			}
+			if (found == 1) break;
+			
+			if ((subterm->coeff % term2->coeff) != 0) break;
+			int coeff = subterm->coeff / term2->coeff;
+			int power = subterm->power - term2->power;
+			insertPlynTerm(&div, coeff, power);
 		}
-		expr1 = expr1->next;
-		expr2 = expr2->next;
+		p = p->next;
+		q = q->next;
+	
+		i++;
 	}
-
+	freeList(&mul);
+	freeList(&sub);
+	if (found == 0) {
+		freeList(&div);
+		return NULL;
+	}
+	simplifyPlynExpr(&div);
 	return div;
 }
 
+/*	w.i.p function to simplify polynomial expressions
+ *	as of now, it works fine if exponents are in decreasing order and have close values
+ *	will not work if it doesn't have these conditions
+ *	currently working on fixing that
+*/
 struct lnode *simplifyPlynExpr(struct lnode **expr)
 {
+	/*
+	int i = 1;
+	struct lnode *p = *expr;
+	struct plyn *data = (struct plyn*)p->data;
+	struct lnode *smpl_expr = NULL;
+
+	int biggest_pow = data->power;
+	while (p != NULL) {
+		struct plyn *data = (struct plyn*)p->data;
+		if (biggest_pow < data->power) {
+			biggest_pow = data->power;
+		}
+		p = p->next;
+		i++;
+	}
+	p = *expr;
+
+	int *pow_arr = malloc(sizeof(int) * MAX_COEFF);
+	int *coeff_arr = malloc(sizeof(int) * MAX_COEFF);
+
+	i = 0;
+	p = *expr;
+	while (p != NULL) {
+		data = (struct plyn*)p->data;
+		pow_arr[i] = data->power;
+		p = p->next;
+		i++;
+	}
+
+	int size = i;
+
+	sortArray(pow_arr, MAX_COEFF);
+
+	p = *expr;
+	while (p != NULL) {
+		i = 0;
+		data = (struct plyn*)p->data;
+		while (i != MAX_COEFF) {
+			if (data->power == pow_arr[i]) {
+				coeff_arr[i] += data->coeff;
+			}
+			i++;
+		}
+		p = p->next;
+	}
+
+	sortArray(coeff_arr, MAX_COEFF);
+
+	i = 0;
+	p = *expr;
+	while (p != NULL && i < size) {
+		if (smpl_expr == NULL)
+			smpl_expr = createPlynExpr(coeff_arr[i], pow_arr[i]);
+		else
+			insertPlynTerm(&smpl_expr, coeff_arr[i], pow_arr[i]);
+		
+		p = p->next;
+		i++;
+	}
+
+	i = 1;
+	p = smpl_expr;
+	while (p != NULL) {
+		data = (struct plyn*)p->data;
+		if (data->coeff == 0) {
+			deleteAtLpos(&smpl_expr, i);
+			p = smpl_expr;
+			i = 1;
+			continue;
+		}
+		p = p->next;
+		i++;
+	}
+
+	freeList(expr);
+	*expr = smpl_expr;
+	return smpl_expr;
+	*/
 	int i = 1;
 	struct lnode *p = *expr;
 	struct plyn *data = (struct plyn*)p->data;
@@ -236,6 +411,7 @@ struct lnode *simplifyPlynExpr(struct lnode **expr)
 		if (data->coeff == 0) {
 			deleteAtLpos(&smpl_expr, i);
 			p = smpl_expr;
+			i = 1;
 			continue;
 		}
 		p = p->next;
@@ -245,6 +421,19 @@ struct lnode *simplifyPlynExpr(struct lnode **expr)
 	freeList(expr);
 	*expr = smpl_expr;
 	return smpl_expr;
+}
+
+void sortArray(int *array, int n) 
+{
+	for (int i = 0; i < n - 1; i++) {
+		for (int j = 0; j < n - i - 1; j++) {
+			if (array[j] < array[j + 1]) {
+				int temp = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = temp;
+			}
+		}
+	}
 }
 
 void printExpr(struct lnode *expr)
