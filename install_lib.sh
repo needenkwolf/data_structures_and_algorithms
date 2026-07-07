4b21015a6c0f59fbb8f83a3706ec131d5f5afbcc
needenkwolf <needenk@gmail.com>
2026-07-06 22:17:23 -0300

    install_lib.sh + .gitignore

--- install_lib.sh
+++ install_lib.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 make
-sudo cp libdatastrucs.so /usr/local/lib/datastrucs/libdatastrucs.so
+sudo mkdir -p /usr/local/lib/nkextralib/
+sudo mkdir -p /usr/local/include/nkextralib/
+sudo cp libdatastrucs.so /usr/local/lib/nkextralib/libdatastrucs.so
 
 p=include
 for f in $(ls $p); do
 	if [ -f "$p/$f" ]; then
-		sudo cp $p/$f /usr/local/include/datastrucs/$f
+		sudo cp $p/$f /usr/local/include/nkextralib/$f
 	fi
 done
